d995dfa9281ae6917ffebced012f70e083f52fab
EricM <menaericdaniel@gmail.com>
2022-05-20 07:20:54 -0300

    fix controllers

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-const path = require('path');
-
-module.exports = {
-    Card : (req,res) => res.render(path.resolve('./src/views/products/productCard')),
-    All : (req,res) => res.render(path.resolve('./src/views/products/productAll')),
-    Cart : (req,res) => res.render(path.resolve('./src/views/products/productCart')),
-    add : (req,res) => res.render(path.resolve('./src/views/products/addProducts')),
-    edit : (req,res) => res.render(path.resolve('./src/views/products/editProducts'))
-}
-
-
-      /*
-        const {idProduct} = req.params;
-        const product = products.find(product => product.id === +idProduct);
-        return res.render('productCard',{
-            product
-        })*/
-=======
 const products = require('../data/products');
 const category = require('../data/categories');
 const path = require('path');
@@ -103,4 +84,3 @@ module.exports = {
         return res.redirect('/products/All');
     }
 }
->>>>>>> develop
--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,10 +1,4 @@
 const path = require('path');
-<<<<<<< HEAD
-
-module.exports = {
-    login: (req,res) => res.render(path.resolve('./src/views/users/login')),
-    register: (req,res) => res.render(path.resolve('./src/views/users/register'))
-=======
 const fs = require('fs');
 
 const users = require('../data/users.json');
@@ -12,5 +6,4 @@ const users = require('../data/users.json');
 module.exports = {
     login: (req,res) => res.render('users/login'),
     register: (req,res) => res.render('users/register')
->>>>>>> develop
 }
